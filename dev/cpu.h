94b6c45589866ec0cd1f13134a7acb9383d42384
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2019-10-06 12:42:20 +0200

    Cleaning

--- dev/cpu.h
+++ dev/cpu.h
@@ -18,15 +18,14 @@ char* cpu_get_vendor(char buffer[16]);
 // Halts the processor, there's no coming back from this!
 inline void halt();
 
+static inline uint8_t inportb(uint16_t _port);
+static inline void    outportb(uint16_t _port, uint8_t _data);
 
-inline uint8_t inportb(uint16_t _port);
-inline void    outportb(uint16_t _port, uint8_t _data);
+static inline uint16_t inportw(uint16_t _port);
+static inline void     outportw(uint16_t _port, uint16_t _data);
 
-inline uint16_t inportw(uint16_t _port);
-inline void     outportw(uint16_t _port, uint16_t _data);
-
-inline uint32_t inportd(uint16_t _port);
-inline void     outportd(uint16_t _port, uint32_t _data);
+static inline uint32_t inportd(uint16_t _port);
+static inline void     outportd(uint16_t _port, uint32_t _data);
 
 // Enables interrupts
 static inline void interrupts();
